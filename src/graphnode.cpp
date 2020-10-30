52777cbb88ad41b2585dc09e424c393ba921b171
Tony Kerstiens <tony.kerstiens@gmail.com>
2020-10-30 15:06:30 -0600

    Task 1 completed

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // delete _chatBot;  Task 0 --> this seems to be the second time the same object is deleted
 
     ////
     //// EOF STUDENT CODE
